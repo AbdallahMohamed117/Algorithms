957fd42654e8a9d7ee196648c70653a49d29e4d0
3bdllllllllllllllla <pedrito1172341@gmail.com>
2025-08-22 04:05:01 +0300

    merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+        public static void main(String [] args) {
+            int [] sorted = {1,2,3,4,5,6};
+            int [] toSort = {5,3,2,6,4,1,3,7};
+            dp.Lecture1.mergeSort(toSort,0,toSort.length-1);
+            for(int i : toSort) {
+                System.out.print(i + " ");
+            }
+
         }
-    }
 }
--- src/dp/Lecture1.java
+++ src/dp/Lecture1.java
@@ -1,5 +1,58 @@
 package dp;
 
-public class DP {
+import java.util.ArrayList;
+
+public class Lecture1 {
+
+    public static void mergeSort(int []arr,int s,int e) {
+        if(s < e) {
+            int m = (s+ (e-1)) /2;
+
+            mergeSort(arr,s,m);
+            mergeSort(arr,m+1,e);
+
+            merge(arr,s,e,m);
+        }
+    }
+
+    private static void merge(int [] arr,int s,int e,int m) {
+
+        int l = m-s +1;
+        int r = e-m;
+
+        int[] arr1 = new int [l];
+        int[] arr2 = new int[r];
+
+        for(int i = 0; i <l;++i) {
+            arr1[i] = arr[i+s];
+        }
+        for(int i = 0; i < r;++i) {
+            arr2[i] = arr[i+m+1];
+        }
+        int i=0, j = 0;
+        int k = s;
+        while(i< l && j<r) {
+            if(arr1[i] <= arr2[j]) {
+                arr[k] = arr1[i];
+                ++i;
+            }
+            else if(arr2[j] < arr1[i]) {
+                arr[k] = arr2[j];
+                ++j;
+            }
+            ++k;
+        }
+
+        while(i<l) {
+            arr[k] = arr1[i];
+            ++i;++k;
+        }
+
+        while(j<r) {
+            arr[k] = arr2[j];
+            ++j;++k;
+        }
+
+    }
 
 }
