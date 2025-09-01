395b951209f396202f57a4bb2ed4d532d8fa54bd
007 <bodygamer1003@gmail.com>
2025-09-02 01:01:08 +0300

    Rod Cutting

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import java.util.HashMap;
 public class Main {
         public static void main(String [] args) {
-            int lis = dp.LIS.lis("carbohydrate");
-            System.out.println(lis);
+            int l = 7;
+            int[] value = {0,1,10,13,18,20,31,32};
+            int max = dp.RodCutting.rod(value);
+            System.out.println(max);
         }
 }
