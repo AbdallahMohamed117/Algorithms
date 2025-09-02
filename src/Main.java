28f7c1748cbdcb5103833b293563e8fd7934cb58
3bdllllllllllllllla <pedrito1172341@gmail.com>
2025-09-02 09:02:28 +0300

    Finished PS8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import java.util.HashMap;
 public class Main {
         public static void main(String [] args) {
-            int[] t = {1,3,7,-6,5,-9,0,5,7};
-            int days = dp.PS8.ss(t);
-            System.out.println(t.length-days);
+            char [][] forest = {
+                    {'E', 'E', 'M', 'E', 'T'},
+                    {'T', 'E', 'T', 'M', 'E'},
+                    {'E', 'M', 'E', 'E', 'E'},
+                    {'E', 'T', 'E', 'M', 'E'},
+                    {'M', 'E', 'E', 'T', 'E'}
+            };
+            int paths = dp.PS8.plum(forest);
+            System.out.println(paths);
         }
 }
--- src/dp/PS8.java
+++ src/dp/PS8.java
@@ -31,4 +31,39 @@ public class PS8 {
         }
         return x[0][0];
     }
+
+    public static int plum(char[][] forest) {
+        int n = forest[0].length;
+        int[][] dp = new int[n][n];
+        int [][] k = new int[n][n];
+        for(int i = 0; i < n;++i) {
+            Arrays.fill(k[i],Integer.MIN_VALUE);
+        }
+        for(int i = 0;i<n;++i) {
+            for(int j = 0;j<n;++j) {
+                if(forest[i][j]=='T')
+                    continue;
+                else {
+                    if(i ==0 && j == 0) {
+                        dp[0][0] = 1;
+                        k[0][0] = 0;
+                    }
+                    else {
+                        if(i ==0 || j == 0) {
+                            dp[i][j] = 1;
+                            k[i][j] = Integer.MIN_VALUE;
+                        }
+                        else {int m = forest[i][j] =='M' ? 1 :0;
+                        k[i][j] = m + Math.max(k[i-1][j],k[i][j-1]);
+                        int x1 = ((m + k[i-1][j]) == k[i][j]) ?  dp[i-1][j]:0;
+                        int x2 = ((m + k[i][j-1]) == k[i][j]) ? dp[i][j-1]:0 ;
+                        dp[i][j] = x1+x2;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[n-1][n-1];
+    }
 }
