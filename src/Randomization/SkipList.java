4d1bca89f239f689a0a3cda1919d7a21968c4436
3bdllllllllllllllla <pedrito1172341@gmail.com>
2025-09-25 09:25:52 +0300

    modified Node, implemented exist

--- src/Randomization/SkipList.java
+++ src/Randomization/SkipList.java
@@ -3,25 +3,36 @@ import java.util.Random;
 public class SkipList {
     private class Node{
         int value;
-        Node[] forward;
+        Node next;
+        Node below;
 
-        public Node(int v,int level) {
+        public Node(int v) {
             value = v;
-            forward = new Node[level+1];
+            next = null;
+            below = null;
         }
     }
 
     private Node head;
-    private int levelCap;
     private int level;
     private Random random;
 
     public SkipList() {
-        levelCap = 16;
         level = 0;
-        head = new Node(Integer.MIN_VALUE,levelCap);
+        head = new Node(Integer.MIN_VALUE);
         random = new Random();
     }
 
-
+    public Node insert(int v){}
+    public boolean exist(int v){
+        Node n = head;
+        while(n.below != null) {
+            n = n.below;
+            while(v >= n.next.value) {
+                n = n.next;
+            }
+        }
+        return n.value == v;
+    }
+    public Node delete(int v) {}
 }
